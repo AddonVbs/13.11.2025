533e5d09cef5a9107b534c791119160c3e8e4b01
JX <102360273+AddonVbs@users.noreply.github.com>
2025-11-13 13:44:34 +0700

    add

--- internal/hendler/LinksHendler.go
+++ internal/hendler/LinksHendler.go
@@ -1,1 +1,22 @@
 package hendler
+
+import (
+	"modul/internal/ServiceLinks"
+	"net/http"
+)
+
+type ServiceLinskHendler struct {
+	service ServiceLinks.LinksServiceInterface
+}
+
+func NewLinksServiHendler(s ServiceLinks.LinksServiceInterface) *ServiceLinskHendler {
+	return &ServiceLinskHendler{service: s}
+}
+
+func (h *ServiceLinskHendler) GetLinkByID(w http.ResponseWriter, r *http.Request) {
+	panic("ff")
+}
+
+func (h *ServiceLinskHendler) GetLink(w http.ResponseWriter, r *http.Request) {
+	panic("ff")
+}
