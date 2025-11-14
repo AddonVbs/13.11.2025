d7610c9b0d42f17d5945f146a2593e17e8e4a8f1
JX <102360273+AddonVbs@users.noreply.github.com>
2025-11-14 18:40:53 +0700

    updata

--- internal/models/orm.go
+++ internal/models/orm.go
@@ -2,6 +2,6 @@ package models
 
 type Links struct {
 	ID     int    `json:"links_num"`
-	URL    string `json:"url"` // "url":"status"
+	URL    string `json:"url"`
 	Status string `json:"status"`
 }
